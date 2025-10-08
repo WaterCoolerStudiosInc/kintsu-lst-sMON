242b9ee9e224d36da7662a25165b98ccd70b7c36
Brandon <brandon@kintsu.xyz>
2025-10-08 12:04:56 -0400

    update dependencies
    update solidity 0.8.26 to 0.8.30

--- src/token-gate/KintsuBetaERC1155.sol
+++ src/token-gate/KintsuBetaERC1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.26;
+pragma solidity 0.8.30;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
--- test/StakerFaker.sol
+++ test/StakerFaker.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.26;
+pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/src/Test.sol";
 import {IPrecompile} from "../src/precompile/Staker.sol";
