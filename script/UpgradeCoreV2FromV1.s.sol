0e02ea836cf8ef75305212a81d8f77b1bd6b1f3d
Brandon <brandon@kintsu.xyz>
2025-11-25 17:53:13 -0500

    upgrade script for v2

--- script/UpgradeCoreV2FromV1.s.sol
+++ script/UpgradeCoreV2FromV1.s.sol
@@ -2,36 +2,42 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/src/Script.sol";
-import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "../src/StakedMonadV2.sol";
 
 /**
- * @notice Upgrades StakedMonad proxy to a new implementation
- * @dev StakedMonad implementation must have already been deployed
+ * @notice Upgrades StakedMonad proxy from V1 to V2
  * @dev These environment variables must be set:
  *     - PRIVATE_KEY - Deploying address private key
- * @custom:example forge script UpgradeCoreProxy --sig "run(address)" $STAKED_MONAD_IMPL
+ * @custom:example forge script UpgradeCoreV2FromV1
  */
-contract UpgradeCoreProxy is Script {
-    function run(address newImplementation) external {
+contract UpgradeCoreV2FromV1 is Script {
+    function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         console.log("Wallet: %s", vm.addr(deployerPrivateKey));
 
         vm.startBroadcast(deployerPrivateKey);
 
-        require(newImplementation.code.length > 0, "Invalid StakedMonad implementation");
-        console.log("Using new StakedMonad implementation: %s", newImplementation);
+        // Deploy new implementation
+        address newImplementation = address(new StakedMonadV2());
+        console.log("StakedMonadV2 implementation deployed to: %s", address(newImplementation));
+
+        require(newImplementation.code.length > 0, "Invalid StakedMonadV2 implementation");
+        console.log("Using new StakedMonadV2 implementation: %s", newImplementation);
 
         // Upgrade current proxy
         address currentProxyAddress = getDeploymentAddress("StakedMonad");
         UUPSUpgradeable currentProxy = UUPSUpgradeable(currentProxyAddress);
         console.log("Upgrading proxy found at: %s", currentProxyAddress);
 
-        currentProxy.upgradeToAndCall(newImplementation, "");
+        currentProxy.upgradeToAndCall(
+            newImplementation,
+            abi.encodeCall(StakedMonadV2.initializeFromV1, ())
+        );
 
         vm.stopBroadcast();
 
         // Update `abiSource` with new contract name for future versions
-        upgradeArtifacts("StakedMonad", "StakedMonad");
+        upgradeArtifacts("StakedMonad", "StakedMonadV2");
     }
 
     function getDeploymentAddress(string memory contractName) internal view returns (address) {
